d8d76eb44e7f946a727fd0e5c61c8dfdf6cb9c47
hyeri <tina724dev@gmail.com>
2020-10-17 10:15:58 +0900

    publish3

--- src/components/PostListView.js
+++ src/components/PostListView.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Image } from 'semantic-ui-react'
+import { Grid, Image, Icon } from 'semantic-ui-react'
 
 class PostListView extends Component {
 
@@ -10,24 +10,25 @@ class PostListView extends Component {
                 <Grid>
                     <Grid.Row columns={3}>
                         <Grid.Column>
-                            <Image src='/images/wireframe/image.png' size="medium"/>
+                            <Image src='/images/wireframe/image1.jpg' size="medium"/>
+                            <Icon name='heart outline' size='large' color="grey"/>
                         </Grid.Column>
                         <Grid.Column>
-                            <Image src='/images/wireframe/image.png' size="medium"/>
+                            <Image src='/images/wireframe/image2.jpg' size="medium"/>
                         </Grid.Column>
                         <Grid.Column>
-                            <Image src='/images/wireframe/image.png' size="medium"/>
+                            <Image src='/images/wireframe/image3.jpg' size="medium"/>
                         </Grid.Column>
                     </Grid.Row>
                     <Grid.Row columns={3}>
                         <Grid.Column>
-                            <Image src='/images/wireframe/image.png' size="medium"/>
+                            <Image src='/images/wireframe/image1.jpg' size="medium"/>
                         </Grid.Column>
                         <Grid.Column>
-                            <Image src='/images/wireframe/image.png' size="medium"/>
+                            <Image src='/images/wireframe/image2.jpg' size="medium"/>
                         </Grid.Column>
                         <Grid.Column>
-                            <Image src='/images/wireframe/image.png' size="medium"/>
+                            <Image src='/images/wireframe/image3.jpg' size="medium"/>
                         </Grid.Column>
                     </Grid.Row>
                 </Grid>
